f4cfc34623c88ca0dbc84704058bd4e6d93a92b1
ynagendra123 <72516164+ynagendra123@users.noreply.github.com>
2020-10-08 19:51:20 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,5 +3,10 @@ def buildApp() {
 	echo "Running the script from external world"
 	echo "where r u from"
 			  }
+def testApp() {
+	echo "testing the application"
+	echo "Running the script from test"
+	echo "Andhra Pradesh"
+			  }
 
 return this
